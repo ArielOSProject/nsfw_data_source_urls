ad193b3e2df9d2cad51faabef1bb28ca2db99627
Sava Mikalacki <sava@codekrafters.com>
2020-09-03 17:44:43 +0200

    Update scripts to download images

--- scripts/2_download_from_urls_.sh
+++ scripts/2_download_from_urls_.sh
@@ -4,20 +4,26 @@ scripts_dir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 base_dir="$(dirname "$scripts_dir")"
 raw_data_dir="$base_dir/raw_data"
 
-declare -a class_names=(
-	"neutral"
-	"drawings"
-	"sexy"
-	"porn"
-	"hentai"
-	)
-
-for cname in "${class_names[@]}"
+for cname in $raw_data_dir/* ;
 do
-	urls_file="$raw_data_dir/$cname/urls_$cname.txt"
-	images_dir="$raw_data_dir/$cname/IMAGES"
+	class_dir_name="${cname##*/}"
+	echo "class dir name $class_dir_name"
+	urls_file="$raw_data_dir/$class_dir_name/urls_$class_dir_name.txt"
+	#images_dir="$raw_data_dir/$cname/IMAGES"
+	images_dir="$base_dir/IMAGES/$class_dir_name/"
 	mkdir -p "$images_dir"
-	echo "Class: $cname. Total # of urls: $(cat $urls_file | wc -l)"
+	echo "Class: $class_dir_name. Total # of urls: $(cat $urls_file | wc -l)"
 	echo "Downloading images..."
 	xargs -n 20 -P 8 wget -nc -q --timeout=5 --tries=2 -P "$images_dir" < "$urls_file"
+
+	# url_file_name="${url_file##*/}"
+	# file_extension="${url_file_name##*.}"
+	# filename="${url_file_name%.*}"
+	# urls_file="$source_urls_dir/$url_file_name"
+	# echo "$urls_file"
+	# images_dir="$base_dir/IMAGES/$filename"
+    # mkdir -p "$images_dir"
+	# echo "Class: $filename. Total # of urls: $(cat $urls_file | wc -l)"
+	# echo "Downloading images..."
+	# xargs -n 20 -P 8 wget -nc -q --timeout=5 --tries=2 -P "$images_dir" < "$urls_file"
 done
